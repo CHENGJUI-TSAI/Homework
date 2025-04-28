31601978494f8b662ae81d3eb2d5526a35a1b5b5
LIOU JHONG EN <41243249@nfu.edu.tw>
2025-04-28 19:01:27 +0800

    Update sorts.h

--- homework/src/project/Data/sorts.h
+++ homework/src/project/Data/sorts.h
@@ -4,8 +4,7 @@
 
 #include <vector>
 
-// 不要在 header 裡用 using namespace std;
-// 函式宣告改用 std::vector<int> 全名。
+
 
 void insertionSort(std::vector<int> arr);
 void quickSort(std::vector<int> arr);
@@ -13,4 +12,4 @@ void mergeSort(std::vector<int> arr);
 void heapSort(std::vector<int> arr);
 void compositeSort(std::vector<int> arr);
 
-#endif // SORTS_H
+#endif 
